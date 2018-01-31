5c571c511bf3efd8a6185b889490654a97401a4e
C00LSkY <c00lspam1@gmail.com>
2018-01-31 19:17:25 +0300

    Выделен вспомогательные классы и метод

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from group import Group
 
 def is_alert_present(wd):
     try:
@@ -17,18 +18,18 @@ class test_add_group(unittest.TestCase):
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # create group
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("%s" % name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
 
     def open_group_page(self, wd):
@@ -46,28 +47,28 @@ class test_add_group(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def test_test_add_group(self):
+    def test_add_group(self):
         success = True
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_group(wd, name="test_group", header="тестовая группа", footer="группа 2018")
+        self.create_group(wd, Group(name="test_group", header="тестовая группа", footer="группа 2018"))
         self.open_group_page(wd)
         self.logout(wd)
 
-    def test_test_add_group(self):
+    def test_empty_group(self):
         success = True
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.open_group_page(wd)
         self.logout(wd)
 
 
-     def tearDown(self):
+    def tearDown(self):
         self.wd.quit()
 
 if __name__ == '__main__':
