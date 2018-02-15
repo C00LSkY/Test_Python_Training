941dd860c35c360ebb2e320de5b84b040d3a132b
C00LSkY <c00lspam1@gmail.com>
2018-02-15 19:23:29 +0300

    Все тесты переделаны на pytest выполнено ДЗ№4

--- test_add_new_user.py
+++ test_add_new_user.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
 import pytest
 from contacts import Anketa
 from application_user import Application_user
@@ -8,19 +7,19 @@ from application_user import Application_user
 
 @pytest.fixture()
 def app(request):
-    fixture = Application()
+    fixture = Application_user()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 
-def test_add_new_user(self):
-    self.login(username="admin", password="secret")
-    self.add_new_user(Anketa(firstname="Вася", midlename="Петрович", lastname='Пупкин', nickname='Пупка',
+def test_add_new_user(app):
+    app.login(username="admin", password="secret")
+    app.add_new_user(Anketa(firstname="Вася", midlename="Петрович", lastname='Пупкин', nickname='Пупка',
                                      company='Ромашка', address='Москва', home_tel='79999999999',
                                      mobile_tel='78888888888', work_tel='77777777777', email='pypka@mail.ru',
                                      byear='1990', address2='Москва 2'))
-    self.logout()
+    app.logout()
 
 
 
