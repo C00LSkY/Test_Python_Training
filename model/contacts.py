5fb87df762fa7ec8d504211fb90a0a58978ed509
C00LSkY <c00lspam1@gmail.com>
2018-02-25 20:42:33 +0300

    Выполнено ДЗ№9

--- model/contacts.py
+++ model/contacts.py
@@ -2,8 +2,9 @@ __author__ = "C00LSkY"
 
 class Anketa:
 
-    def __init__(self, firstname=None, midlename=None, lastname=None, nickname=None, company=None, address=None,
-                 home_tel=None, mobile_tel=None, work_tel=None, email=None,byear=None, address2=None):
+    def __init__(self, firstname = None, midlename = None, lastname = None, nickname = None, company = None,
+                 address = None, home_tel = None, mobile_tel = None, work_tel = None, email = None,byear = None,
+                 address2 = None):
         self.firstname = firstname
         self.midlename = midlename
         self.lastname = lastname
