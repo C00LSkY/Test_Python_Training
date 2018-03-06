026cbe17a0acd15a3317a1bef6bc5692d1173505
C00LSkY <c00lspam1@gmail.com>
2018-03-06 18:11:34 +0300

    добавили списки груп

--- fixture/group.py
+++ fixture/group.py
@@ -1,5 +1,7 @@
 __author__ = "C00LSkY"
 
+from model.group import Group
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -61,6 +63,17 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         return len (wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
+
 
 
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,7 +5,10 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_group = app.group.get_group_list()
     app.group.create(Group(name="test_group", header="тестовая группа", footer="группа 2018"))
+    new_groups = app.group.get_group_list()
+    assert len(old_group) + 1 == len(new_groups)
 
 
 def test_empty_group(app):
