397a5f136fb6f0b2e64a946d122a8100cffceff5
C00LSkY <c00lspam1@gmail.com>
2018-03-20 22:57:06 +0300

    сделаны тесты создания групп с рандомным заполнением данныч

--- test/test_add_new_user.py
+++ test/test_add_new_user.py
@@ -3,10 +3,26 @@ __author__ = "C00LSkY"
 
 
 from model.contacts import Anketa
+import pytest
+import random
+import string
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
 
-def test_add_new_user(app):
+testdata = [
+        Anketa(name=name, header=header, footer=footer)
+        for name in ["", random_string("name", 10)]
+        for header in ["", random_string("header", 20)]
+        for footer in ["", random_string("footer", 20)]
+]
+
+@pytest.mark.parametrize("user", testdata, ids=[repr(x) for x in testdata])
+def test_add_new_user(app, user):
     old_users = app.user.get_user_list()
     user = Anketa(firstname="Вася", midlename="Петрович", lastname='Пупкин', nickname='Пупка',
                                      company='Ромашка', address='Москва', home_tel='79999999999',
