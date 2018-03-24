2878d3f334fe37f23a04af8609e46d3f50a78d88
C00LSkY <c00lspam1@gmail.com>
2018-03-24 21:53:38 +0300

    контрольная точка изменений подгрузки данных

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,7 +3,7 @@ __author__ = "C00LSkY"
 
 from model.group import Group
 import pytest
-from data.add_group import testdata
+from data.groups import testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
